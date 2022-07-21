87904a2abc17886f284a0b45dcada1a491a1c595
Talha <tkz9696@gmail.com>
2022-07-21 14:53:23 +0500

    Setup db and basic queries

--- data.sql
+++ data.sql
@@ -1,5 +1,83 @@
 /* Populate database with sample data. */
+INSERT INTO
+    animals (name)
+VALUES
+    ('Luna');
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO
+    animals (name)
+VALUES
+    ('Daisy');
+
+INSERT INTO
+    animals (name)
+VALUES
+    ('Charlie');
+
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Agumon',
+        DATE '2020-02-03',
+        '0',
+        'true',
+        '10.23'
+    );
+
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Gabumon',
+        DATE '2018-11-15',
+        '2',
+        'true',
+        '8'
+    );
+
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Pikachu',
+        DATE '2021-07-01',
+        '1',
+        'false',
+        '10.23'
+    );
+
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    (
+        'Devimon',
+        DATE '2017-05-12',
+        '0',
+        'true',
+        '11'
+    );
