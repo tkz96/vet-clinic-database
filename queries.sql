be7fc1369cd564e9d0de0c8eca78f407c82f7757
Talha <tkz9696@gmail.com>
2022-07-22 20:49:42 +0500

    add queries

--- queries.sql
+++ queries.sql
@@ -17,3 +17,30 @@ SELECT name FROM animals WHERE neutered;
 SELECT name FROM animals WHERE NOT name = 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SAVEPOINT save_point;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO save_point;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT * FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
