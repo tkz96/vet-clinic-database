628f4d3d49a0411ab1555aa266c20e6a093fe2bc
Talha <tkz9696@gmail.com>
2022-07-23 18:25:11 +0500

    Add schema and data

--- schema.sql
+++ schema.sql
@@ -1,14 +1,33 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg NUMERIC NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg NUMERIC
 );
 
 ALTER TABLE
     animals
 ADD
     species VARCHAR(50);
+
+CREATE TABLE owners (
+    id BIGSERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT 
+);
+
+CREATE TABLE species (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+ALTER TABLE animals
+DROP
+    COLUMN species,
+ADD
+    COLUMN species_id BIGINT REFERENCES species (id),
+ADD
+    COLUMN owner_id BIGINT REFERENCES owners (id);
